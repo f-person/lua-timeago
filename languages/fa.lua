1b80b7cff9415b954631ff2ddd9d7ced54fdc018
Ahmad Reza Enshaee <enshaee.ahmadreza@gmail.com>
2022-12-14 14:35:02 +0330

    fixed a typo.

--- languages/fa.lua
+++ languages/fa.lua
@@ -3,7 +3,7 @@ return {
     minute = {singular = 'یک دقیقه پیش', plural = 'دقایقی پیش'},
     hour = {singular = 'یک ساعت پیش', plural = 'ساعاتی پیش'},
     day = {singular = 'یک روز پیش', plural = 'روزها پیش'},
-    week = {singular = 'یک هفته پیش', plural = 'هفته پیش'},
+    week = {singular = 'یک هفته پیش', plural = 'هفته‌ها پیش'},
     month = {singular = 'یک ماه پیش', plural = 'ماه‌ها پیش'},
     year = {singular = 'یک سال پیش', plural = 'سال‌ها پیش'}
 }
